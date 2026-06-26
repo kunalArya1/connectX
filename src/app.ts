0402e9e1afcc4e167b24a4dc5518297c2c2b5b14
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2026-06-26 08:17:09 +0530

    feat: added routes config

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,30 @@ import dotenv from "dotenv";
 dotenv.config({});
 import express, { type Application } from "express";
 import cors from "cors";
+import path from "node:path";
 import auhtRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import isLoggedIn from "./middlewares/auth.middlewares.js";
+import profileRouter from "./routes/profile.routes.js";
+import connectionRouter from "./routes/connection.routes.js";
+import { apiReference } from "@scalar/express-api-reference";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/openapi.json", (_req, res) => {
+  res.sendFile(path.resolve("src/utils/openapi.json"));
+});
+
+app.use(
+  "/reference",
+  apiReference({
+    theme: "purple",
+    url: "/openapi.json",
+  }),
+);
 
 /**
  * Health Api
@@ -19,5 +37,8 @@ app.get("/", (req, res) => {
   });
 });
 app.use("/auth", auhtRouter);
+app.use("/request", isLoggedIn, connectionRouter);
+app.use("/profile", profileRouter);
+app.use("/user", isLoggedIn, userRouter);
 
 export default app;
