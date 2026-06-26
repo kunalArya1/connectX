21eb12a319611178deaa243d6e241fdc926788df
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2026-06-26 08:17:34 +0530

    feat: routes config cahnges

--- src/routes/connection.routes.ts
+++ src/routes/connection.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { reviewRequest, sendRequest } from "../controllers/connection.controllers.js";
+import {
+  reviewRequest,
+  sendRequest,
+} from "../controllers/connection.controllers.js";
 
 const router: Router = Router();
 
-router.route("/request/send/:status/:toUserId").post(sendRequest);
-router.route("/request/review/:status/:requestId").post(reviewRequest);
+router.route("/send/:status/:toUserId").post(sendRequest);
+router.route("/review/:status/:requestId").post(reviewRequest);
 
 export default router;
